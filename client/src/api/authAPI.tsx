768fbfb525c6c21c710559c38d01220bed9dd509
lwebert <lauren@weberts.org>
2025-03-05 23:14:20 -0600

    Client side authentication

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,13 +2,27 @@ import { UserLogin } from '../interfaces/UserLogin';
 
 const login = async (userInfo: UserLogin) => {
 	// TODO: make a POST request to the login route
-	console.log('authAPI login userInfo: ', userInfo);
-	// try {
-	// } catch (err) {
-	//   console.log('Error from user login: ', err);
-	// }
+	try {
+		const response = await fetch('/auth/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(userInfo),
+		});
 
-	return { token: 'asdf' };
+		if (!response.ok) {
+			//OK status = 200-299
+			const errorData = await response.json();
+			throw new Error(`Error: ${errorData.message}`);
+		}
+
+		const data = await response.json();
+		return data;
+	} catch (err) {
+		console.log('Error from user login: ', err);
+		return Promise.reject('Could not fetch user login info.');
+	}
 };
 
 export { login };
